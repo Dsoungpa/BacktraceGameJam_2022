1aca3c4981423ab9f8aab4fd7df6c9d363121ef4
Dylan Soungpanya <dsoungpa@ucsc.edu>
2022-06-24 20:08:37 -0700

    Got the starting platform spawn working

---
+++ Backtrace Game Jam 2022/Assets/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(transform.position.x, target.position.y + 5f, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
+    }
+}

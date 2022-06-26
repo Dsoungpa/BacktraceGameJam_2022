508e239b72557377e688a1f136157358b48fa06e
Dylan Soungpanya <dsoungpa@ucsc.edu>
2022-06-25 17:58:42 -0700

    Added Background and Implemented the Infinite

--- Backtrace Game Jam 2022/Assets/Scripts/CameraController.cs
+++ Backtrace Game Jam 2022/Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        size = 13f;
+        size = 24f;
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -18,19 +18,23 @@ public class CameraController : MonoBehaviour
         //print("Transform Position: " + (transform.position.y));
         // print("Background1 Position: " + background1.position.y);
         // print("Background2 Position: " + background2.position.y);
-        // // Switch backgrounds
-        // // if you go higher than background 1 put it on top
-        // if(transform.position.y >= background2.position.y){
-        //     print(size);
-        //     print(background2.position.y + size);
-        //     background1.position = new Vector3(background1.position.x, background2.position.y + size, background1.position.z);
-        //     SwitchBackground();
-        // }
-        // // if you go lower than background 1 bring background 2 down
-        // if(transform.position.y < background1.position.y){
-        //     background2.position = new Vector3(background2.position.x, background1.position.y - size, background2.position.z);
-        //     SwitchBackground();
-        // }
+        
+        if(transform.position.y > 24f){
+
+            // Switch backgrounds
+            // if you go higher than background 1 put it on top
+            if(transform.position.y >= background2.position.y){
+                print(size);
+                print(background2.position.y + size);
+                background1.position = new Vector3(background1.position.x, background2.position.y + size, background1.position.z);
+                SwitchBackground();
+            }
+            // // if you go lower than background 1 bring background 2 down
+            if(transform.position.y < background1.position.y){
+                background2.position = new Vector3(background2.position.x, background1.position.y - size, background2.position.z);
+                SwitchBackground();
+            }
+        }
     }
     private void SwitchBackground(){
             Transform temp = background1;
